01771c80d3ace345cd764a568084698d2d0c3d77
DESKTOP-QCO0JC1\WLX <1018482705@qq.com>
2017-08-13 20:12:10 +0800

    no message

---
+++ 8.13.js
@@ -0,0 +1,14 @@
+function displayInfo(args) {
+    var output = "";
+    if (typeof args.name == "string") {
+        output += "Name:" + args.name + "\n";
+    }
+    if (typeof args.age == "number") {
+        output += "Age:" + args.age + "\n";
+    }
+    alert(output);
+}
+displayInfo({
+    name: "Nicholas",
+    age: 29
+});
